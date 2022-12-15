efb59bafecb5909a1b78ec73591f033ce1217880
Gustavo López <git.gustavolopez.xyz@gmail.com>
2022-12-15 10:28:40 -0400

    Mejora legibilidad de acciones

--- src/components/Acciones.tsx
+++ src/components/Acciones.tsx
@@ -5,11 +5,14 @@ export function Acciones() {
   const { predecirTorneo, resetOctavos, resetCuartos, resetSemi } = useTorneoContext();
 
   return (
-    <div className="grid grid-flow-dense grid-cols-2 gap-2 md:grid-cols-4 lg:flex lg:space-x-4">
-      <Button onClick={predecirTorneo}>Predecir puestos</Button>
-      <Button onClick={resetOctavos}>Reset a octavos</Button>
-      <Button onClick={resetCuartos}>Reset a cuartos</Button>
+    <div className="grid grid-flow-dense grid-cols-2 gap-2 md:grid-cols-4 lg:flex lg:justify-between lg:space-x-4">
+      <Button className="col-span-2 mb-2 md:col-span-4 lg:mb-0" onClick={predecirTorneo}>
+        Predecir puestos
+      </Button>
+      <div className="flex-grow-1 hidden flex-shrink-0 lg:block"></div>
       <Button onClick={resetSemi}>Reset a semifinal</Button>
+      <Button onClick={resetCuartos}>Reset a cuartos</Button>
+      <Button onClick={resetOctavos}>Reset a octavos</Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,21 @@
 import { ReactNode } from "react";
 
 interface Props {
+  className?: string;
   children: ReactNode;
   [x: string]: unknown;
 }
 
 export function Button(props: Props) {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
 
   return (
-    <button className="block bg-gray-900 p-2 font-mono text-sm uppercase text-gray-300 md:p-4 md:text-base" {...rest}>
+    <button
+      className={
+        "block bg-gray-900 p-2 font-mono text-sm uppercase text-gray-300 md:p-4 md:text-base " + (className ?? "")
+      }
+      {...rest}
+    >
       {children}
     </button>
   );
